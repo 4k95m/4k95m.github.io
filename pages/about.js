db06b8815aa3595b1baad64df6d65d849ecb9bdd
4k95m <4k95m.jp@gmail.com>
2023-01-29 01:09:44 -0500

    dynamic header & title (useRouter)

--- pages/about.js
+++ pages/about.js
@@ -1,17 +1,22 @@
 import Head from 'next/head'
+import { useRouter } from 'next/router'
 
-export default () => (
-  <>
-    <Head>
-      <title>4k95m - about</title>
-    </Head>
-    <h1>4k95m.github.io/about</h1>
-    <hr />
-    <br />
-      <div>About me
-        <li><a href="https://twitter.com/4k95m">Twitter</a></li>
-      </div>
-  </>
-)
+export default () => {
+  const router = useRouter()
+  
+  return(
+    <>
+      <Head>
+        <title>{`4k95m - ${(router.pathname).replace("/","")}`}</title>
+      </Head>
+      <h1>4k95m.github.io/about</h1>
+      <hr />
+      <br />
+        <div>About me
+          <li><a href="https://twitter.com/4k95m">Twitter</a></li>
+        </div>
+    </>
+  )
+}
 
 
--- pages/writeups.js
+++ pages/writeups.js
@@ -1,19 +1,23 @@
 import Head from'next/head'
+import { useRouter } from 'next/router'
 
-export default () => (
-  <>
-    <Head>
-      <title>4k95m - writeups</title>
-    </Head>
-    <h1>4k95m.github.io/writeups</h1>
-    <hr />
-    <br />
-      <div>Writeups
-	<li><a href="https://material-paneer-4f8.notion.site/TsukuCTF22-Writeup-423203c585444494bdbc2dec05fe08b2">TsukuCTF22</a></li>
-        <li><a href="https://material-paneer-4f8.notion.site/UECTF2022-Writeup-424cfdca7e6e4d0e83faefc1a7bcea45">UECTF2022</a></li>
-        <li><a href="https://material-paneer-4f8.notion.site/taskctf22-Writeup-b66e944e918a42ce9d86cce35199d95d">taskctf22</a></li>
-	<li><a href="/writeups/HHC2022.pdf">SANS Holiday Hack Challenge 2022</a></li>
-      </div>
-  </>
-)
-
+export default () => {
+  const router = useRouter()
+  // console.log((router.pathname).replace("/",""))
+  return(
+    <>
+      <Head>
+        <title>{`4k95m - ${(router.pathname).replace("/","")}`}</title>
+      </Head>
+      <h1>4k95m.github.io{router.pathname}</h1>
+      <hr />
+      <br />
+        <div>Writeups
+	  <li><a href="https://material-paneer-4f8.notion.site/TsukuCTF22-Writeup-423203c585444494bdbc2dec05fe08b2">TsukuCTF22</a></li>
+          <li><a href="https://material-paneer-4f8.notion.site/UECTF2022-Writeup-424cfdca7e6e4d0e83faefc1a7bcea45">UECTF2022</a></li>
+          <li><a href="https://material-paneer-4f8.notion.site/taskctf22-Writeup-b66e944e918a42ce9d86cce35199d95d">taskctf22</a></li>
+	  <li><a href="/writeups/HHC2022.pdf">SANS Holiday Hack Challenge 2022</a></li>
+        </div>
+    </>
+  )
+}
